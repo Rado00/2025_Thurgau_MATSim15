79cef0edbbe56691ab5a61a1b699735e363fe558
muaa <corrado.muratori0@gmail.com>
2026-03-22 20:38:04 +0100

    Out Lambda 45

--- abmt2025/autoRun_DRT_parallel_and_analyse.sh
+++ abmt2025/autoRun_DRT_parallel_and_analyse.sh
@@ -17,24 +17,24 @@ DELETE_EVENTS_FILE=true
 BASELINE_PCT="100pct"
 
 
-SIM_ID="Test_DRT_Rej_03" # CHANGE TO RUN PARALLEL SIMS WITH DIFFERENT SETTINGS
-FLEET_FILE="25_drt_20_8.xml"
-SHAPE_FILE="25_ShapeFile.shp"
+SIM_ID="Lambda_45" # CHANGE TO RUN PARALLEL SIMS WITH DIFFERENT SETTINGS
+FLEET_FILE="10_drt_1_8.xml"
+SHAPE_FILE="10_ShapeFile.shp"
 
 ########################## DRT PARAMETERS (for swissRail_08 and 10 config) ###########################################
 # DRT Fare (passed to Java as system properties)
-DRT_FARE_CHF="2"           # Fixed constant price DRT (CHF)
-DRT_FARE_CHF_KM="0.7"        # Per-km price DRT (CHF/km)
+DRT_FARE_CHF="10"           # Fixed constant price DRT (CHF)
+DRT_FARE_CHF_KM="0"        # Per-km price DRT (CHF/km)
 
 # DRT Operational Constraints (substituted in config XML)
-REJECT_IF_CONSTRAINTS_VIOLATED="false"   # true = hard constraints, false = soft constraints
+REJECT_IF_CONSTRAINTS_VIOLATED="true"   # true = hard constraints, false = soft constraints
 MAX_WAIT_TIME="1200.0"                    # Max wait time in seconds
 MAX_TRAVEL_TIME_ALPHA="2"                # maxTravelTime = alpha * unsharedRideTime + beta
 MAX_TRAVEL_TIME_BETA="300.0"             # maxTravelTime shift in seconds
 
 # Modal Split Calibration (passed to Java as system properties)
-ALPHA_WALK="2.5"
-ALPHA_BIKE="2.3"
+ALPHA_WALK="2.6"
+ALPHA_BIKE="2.65"
 ALPHA_PT="0"
 ALPHA_CAR="1.7"
 BETA_CAR_CITY="-0.459"
